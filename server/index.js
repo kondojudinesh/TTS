fcee3245760ab88f6b61ae95a3e96daa342c03ea
kondojudinesh <169022340+kondojudinesh@users.noreply.github.com>
2025-07-28 20:40:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(express.json());
 // Routes
 app.use('/api/audio', require('./routes/audioRoutes'));
 app.use('/api/history', require('./routes/historyRoutes'));
+app.get('/', (req, res) => {
+  res.send('✅ Speech-to-Text Backend is running');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
